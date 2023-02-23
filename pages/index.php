6bffedc2653df2ee226b553b3f533f519186f183
Gerónimo <gerolpz01@gmail.com>
2023-02-23 16:52:26 -0300

    terminado index

--- pages/index.php
+++ pages/index.php
@@ -7,13 +7,26 @@ $sql = "SELECT * FROM resturante";
 $query = mysqli_query($conn, $sql);
 
 ?>
-
-<h1>Mira nuestra lista de restaurantes</h1>
-<?php
-while ($row = mysqli_fetch_array($query)) :
-?>
-    <h3><?php echo $row["NombreFantasía"] ?></h3>
-<?php endwhile; ?>
+<div class="d-flex justify-content-center">
+    <div class="box">
+        <h5 class="text-center">Nuestra lista de restaurantes</h5>
+        <hr />
+        <div class="tarjetas-padre card-deck">
+            <?php
+            while ($row = mysqli_fetch_array($query)) :
+            ?>
+                <div class="card text-center" style="width: 18rem;">
+                    <img class="card-img-top" src="<?php echo $row["Logo"] ?>" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $row["NombreFantasía"] ?></h5>
+                        <hr />
+                        <a class="btn btn-dark center" href="./restaurantes_id.php?id=<?php echo $row["Id"] ?>" class="btn btn-primary">Detalle</a>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</div>
 <?php
 require("./base/footer.php")
 ?>
