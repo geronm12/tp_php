b17ad0a9cf50bc56f3034b3506ef98c23020a8a3
Gerónimo <gerolpz01@gmail.com>
2023-02-28 18:17:38 -0300

    terminado sitio, probados CRUD, agregada pantalla de nosotros

--- pages/guia_ubicacion.php
+++ pages/guia_ubicacion.php
@@ -5,46 +5,74 @@ require("./navbar.php");
 <div class="d-flex justify-content-center">
     <div class="box text-center">
         <h5>Instrucciones para obtener la ubicación</h5>
-        <div>
-            <span><b>1</b></span>
-            <p>Ir hasta google y buscar la dirección que deseamos obtener, acto seguido haremos click donde dice "Maps".</p>
-            <img class="tutorial-imagen" src="../img/a.png" />
+        <div class="card">
+            <div class="card-header">
+                <span><b>1</b></span>
+            </div>
+            <div class="card-body">
+                <p>Ir hasta google y buscar la dirección que deseamos obtener, acto seguido haremos click donde dice "Maps".</p>
+                <img class="tutorial-imagen" src="../img/a.png" />
+            </div>
         </div>
         <hr />
-        <div>
-            <span>2</span>
-            <p>Una vez que estemos en la pestaña de maps debemos hacer click en donde dice "Compartir".</p>
-            <img class="tutorial-imagen" src="../img/b.png" />
+        <div class="card">
+            <div class="card-header">
+                <span><b>2</b></span>
+            </div>
+            <div class="card-body">
+                <p>Una vez que estemos en la pestaña de maps debemos hacer click en donde dice "Compartir".</p>
+                <img class="tutorial-imagen" src="../img/b.png" />
+            </div>
         </div>
         <hr />
-        <div>
-            <span>3</span>
-            <p>Desde la pantalla de compartir deberemos hacer click donde dice "Incorporar Mapa".</p>
-            <img class="tutorial-imagen" src="../img/c.png" />
+        <div class="card">
+            <div class="card-header">
+                <span><b>3</b></span>
+            </div>
+            <div class="card-body">
+                <p>Desde la pantalla de compartir deberemos hacer click donde dice "Incorporar Mapa".</p>
+                <img class="tutorial-imagen" src="../img/c.png" />
+            </div>
         </div>
         <hr />
-        <div>
-            <span>4</span>
-            <p>Hacer click donde dice "Copiar HTML".</p>
-            <img class="tutorial-imagen" src="../img/d.png" />
+        <div class="card">
+            <div class="card-header">
+                <span><b>4</b></span>
+            </div>
+            <div class="card-body">
+                <p>Hacer click donde dice "Copiar HTML".</p>
+                <img class="tutorial-imagen" src="../img/d.png" />
+            </div>
         </div>
         <hr />
-        <div>
-            <span>5</span>
-            <p>Pegar el contenido que copiamos en un notepad, bloc de notas, word, etc y
-                copiar nuevamente pero solo el contenido que se encuentra entre comillas ("")
-                luego de donde dice src.
-            </p>
-            <img class="tutorial-imagen" src="../img/e.png" />
+        <div class="card">
+            <div class="card-header">
+                <span><b>5</b></span>
+            </div>
+            <div class="card-body">
+                <p>Pegar el contenido que copiamos en un notepad, bloc de notas, word, etc y
+                    copiar nuevamente pero solo el contenido que se encuentra entre comillas ("")
+                    luego de donde dice src.
+                </p>
+                <img class="tutorial-imagen" src="../img/e.png" />
+            </div>
         </div>
         <hr />
-        <div>
-            <span>6</span>
-            <p>Pegar la url y todo lo que copiamos dentro del campo Ubicación</p>
-            <img class="tutorial-imagen" src="../img/f.png" />
+        <div class="card">
+            <div class="card-header">
+                <span><b>6</b></span>
+            </div>
+            <div class="card-body">
+                <p>Pegar la url y todo lo que copiamos dentro del campo Ubicación</p>
+                <img class="tutorial-imagen" src="../img/f.png" />
+            </div>
         </div>
-        <a class="btn btn-dark" href="./agregar_restaurante.php"> Volver </a>
+        <div class="mt-1">
+            <a class="btn btn-dark" href="./agregar_restaurante.php"> Volver </a>
+        </div>
     </div>
+</div>
+
 </div>
 <?php
 require("./base/footer.php");
--- pages/nosotros.php
+++ pages/nosotros.php
@@ -4,7 +4,32 @@ require("./navbar.php");
 ?>
 <div class="d-flex justify-content-center">
     <div class="box">
-        <h1>Nosotros</h1>
+        <div class="card">
+            <div class="card-header">
+                <b>N</b>osotros
+            </div>
+            <div class="card-body d-flex justify-content-center align-items-center">
+                <img src="../img/foto.jpeg" class="perfil" />
+                <div class="info-personal">
+                    <span>
+                        <b>Nombre</b>
+                    </span>
+                    <br>
+                    <span>
+                        Paola Alejandra Cepeda Sanchez
+                    </span>
+                    <br>
+                    <span>
+                        <b>Email</b>
+                    </span>
+                    <br>
+                    <span>
+                        Pao.cepeda.aleja@gmail.com
+                    </span>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 <?php
